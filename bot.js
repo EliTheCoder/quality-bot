286b28eeb259d264dce6958b5cbf5347b7b3005b
AlexCheese <alexip013@gmail.com>
2019-01-15 22:22:47 -0600

    starting to reuse code and stuff

--- bot.js
+++ bot.js
@@ -49,7 +49,7 @@ bot.on('message', message => {
 			let role = message.guild.roles.find(role => role.name === args[0]);
 			let memb = (args[1] != undefined)
 				? message.guild.members.find(user => user.user.username === args[1])
-				: message.guild.members.find(user => user.user.username === sender.username);
+				: message.member;
 			memb.addRole(role);
 		}
 		catch(err) {
@@ -60,117 +60,65 @@ bot.on('message', message => {
 
 	// delete marked messages
 
+
+	async function purge(check) { // wrapped in async since await only works in it
+		await message.delete(); // delete command message
+
+		if /*(!message.member.roles.some(r=>["GM", "Game Master", "Admin", "Bot Dev", "Mod", "Moderator", "Owner", "gEEK"].includes(r.name)))*/
+		(!message.member.permissions.has(`MANAGE_MESSAGES`)  && (!sender.username == `AlexCheese`)) {
+			message.reply('You do not have permission to manage messages.')
+				.then(msg => {
+					msg.delete(5000);
+					console.log(origin + 'Denied perms.');
+				//	console.log('DEBUG: User Perms:	' + message.member.permissions);
+				//	console.log('DEBUG: User Perms:	' + message.member.roles);
+				})
+				.catch(error => console.log(origin + `Permission Error: ${error}`));
+			return;
+		}
+
+
+
+		if (isNaN(args[0])) {
+			message.reply('Please use a number. \n Usage: ' + prefix + 'ooc [amount]')
+			.then(msg => {
+				msg.delete(8000);
+				console.log(origin + 'Non-numerical input.');
+			})
+			.catch(error => console.log(origin + `Permission Error: ${error}`));
+			return;
+		}
+
+		if ((args[0]) > 100) {
+			message.reply('I cannot delete more than 100 messages at a time. \n Usage: ' + prefix + 'ooc [amount]')
+			.then(msg => {
+				msg.delete(8000);
+				console.log(origin + 'Input greater than 100.');
+			})
+			.catch(error => console.log(origin + `Permission Error: ${error}`));
+			return;
+		}
+
+		const fetched = await message.channel.fetchMessages({limit: args[0]});
+		const filteredr = fetched.filter(input => input["content"].startsWith(check) && input["pinned"] == false);
+		console.log(origin + fetched.size + ' messages found, ' + filteredr.size + ' removed.');
+		message.channel.bulkDelete(filteredr)
+			.catch(error =>	{
+				console.log(`Couldn't delete: ${error}`);
+				message.reply('Some messages could not be deleted because they are older than 14 days.')
+					.then(msg => msg.delete(8000));
+			});
+
+	}
+
 	if (msg.startsWith(prefix + 'OOC')) {
-
-		async function purge() { // wrapped in async since await only works in it
-			await message.delete(); // delete command message
-
-			if /*(!message.member.roles.some(r=>["GM", "Game Master", "Admin", "Bot Dev", "Mod", "Moderator", "Owner", "gEEK"].includes(r.name)))*/
-			(!message.member.permissions.has(`MANAGE_MESSAGES`)  && (!sender.username == `AlexCheese`)) {
-				message.reply('You do not have permission to manage messages.')
-					.then(msg => {
-						msg.delete(5000);
-						console.log(origin + 'Denied perms.');
-					//	console.log('DEBUG: User Perms:	' + message.member.permissions);
-					//	console.log('DEBUG: User Perms:	' + message.member.roles);
-					})
-					.catch(error => console.log(origin + `Permission Error: ${error}`));
-				return;
-			}
-
-
-
-			if (isNaN(args[0])) {
-				message.reply('Please use a number. \n Usage: ' + prefix + 'ooc [amount]')
-				.then(msg => {
-					msg.delete(8000);
-					console.log(origin + 'Non-numerical input.');
-				})
-				.catch(error => console.log(origin + `Permission Error: ${error}`));
-				return;
-			}
-
-			if ((args[0]) > 100) {
-				message.reply('I cannot delete more than 100 messages at a time. \n Usage: ' + prefix + 'ooc [amount]')
-				.then(msg => {
-					msg.delete(8000);
-					console.log(origin + 'Input greater than 100.');
-				})
-				.catch(error => console.log(origin + `Permission Error: ${error}`));
-				return;
-			}
-
-			const fetched = await message.channel.fetchMessages({limit: args[0]});
-			const filteredr = fetched.filter(input => input["content"].startsWith('(') && input["pinned"] == false);
-			console.log(origin + fetched.size + ' messages found, ' + filteredr.size + ' removed.');
-			message.channel.bulkDelete(filteredr)
-				.catch(error =>	{
-					console.log(`Couldn't delete: ${error}`);
-					message.reply('Some messages could not be deleted because they are older than 14 days.')
-						.then(msg => msg.delete(8000));
-				});
-
-		}
-
-		purge();
-
+		purge('(');
 	}
 
-
-/*	if (msg.startsWith(prefix + 'CLEAN')) {
-
-		async function purgeall() { // wrapped in async since await only works in it
-			await message.delete(); // delete command message
-
-			if (!message.member.roles.some(r=>["GM", "Game Master", "Admin", "Bot Dev", "Mod", "Moderator", "Owner"].includes(r.name))) {
-				message.reply('You do not have permission to do this.')
-					.then(msg => {
-						msg.delete(5000);
-						console.log(origin + 'lol, ' + sender.username + ' was denied perms.');
-					})
-					.catch(error => console.log(origin + `Permission Error: ${error}`));
-				return;
-			}
-
-
-
-			if (isNaN(args[0])) {
-				message.reply('Please use a number. \n Usage: ' + prefix + 'clean [amount]')
-				.then(msg => {
-					msg.delete(8000);
-					console.log(origin + 'lol, ' + sender.username + ' didn\'t use a number.');
-				})
-				.catch(error => console.log(origin + `Permission Error: ${error}`));
-				return;
-			}
-
-			if ((args[0]) > 100) {
-				message.reply('I cannot delete more than 100 messages at a time. \n Usage: ' + prefix + 'clean [amount]')
-				.then(msg => {
-					msg.delete(8000);
-					console.log(origin + 'lol, ' + sender.username + ' used a number greater than 100.');
-				})
-				.catch(error => console.log(origin + `Permission Error: ${error}`));
-				return;
-			}
-
-			const fetched = await message.channel.fetchMessages({limit: args[0]});
-			//console.log(fetched)
-			const filterall = fetched.filter(input => input["content"].startsWith('"' || '*') && input["pinned"] == false);
-			console.log(origin + fetched.size + ' messages found, ' + filterall.size + ' marked.');
-			message.channel.bulkDelete(filterall)
-				.catch(error =>	{
-					console.log(`Couldn't delete: ${error}`);
-					message.reply('Some messages could not be deleted because they are older than 14 days.')
-						.then(msg => msg.delete(8000));
-				});
-
-		}
-
-		purgeall();
-
+	if (msg.startsWith(prefix + 'CLEANUP') && sender.username == 'AlexCheese') {
+		purge('');
 	}
-*/
+
 
 	else if (msg.startsWith(prefix + 'PING')) {
 		message.delete();
